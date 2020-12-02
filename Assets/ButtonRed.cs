6d0b652b300c69946fdc2524d9ce8ac972e31635
Geoffrey Hil <g.hill@sae.edu>
2020-12-02 15:41:46 +1100

    Added state machine for dropcube

--- Assets/ButtonRed.cs
+++ Assets/ButtonRed.cs
@@ -5,6 +5,10 @@ using UnityEngine;
 public class ButtonRed : MonoBehaviour
 {
 
+    public enum DropCube {  still, spinning };
+    public DropCube dropCube;
+    
+
     Renderer r;
     // Start is called before the first frame update
     void Start()
@@ -15,8 +19,25 @@ public class ButtonRed : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
+        switch (dropCube)
+        {
+            case DropCube.still:
+                Debug.Log("the dropcube is still");
+                
+                 break;
+
+            case DropCube.spinning:
+                Debug.Log("the dropcube is spinning");
+                transform.Rotate(Vector3.up * 2);
+                break;
+
+        }
+
+
     }
+
+
 
     public void MakeRed()
     {
@@ -26,5 +47,17 @@ public class ButtonRed : MonoBehaviour
     public void MakeBlue()
     {
         r.material.color = Color.blue;
+    }
+
+    public void StartSpinning()
+    {
+        dropCube = DropCube.spinning;
+        MakeBlue();
+    }
+
+    public void StopSpinning()
+    {
+        dropCube = DropCube.still;
+        MakeRed();
     }
 }
